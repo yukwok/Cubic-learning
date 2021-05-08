9041cffc4146669f49b96f25624853672bc2f777
yukwok <billynyk@gmail.com>
2021-05-08 11:51:59 +0800

    blocConsumer draft added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:cubic_learning/cubit/weather_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,6 +13,7 @@ void main() {
 // add the third comments line here if you want
 
 class MyApp extends StatelessWidget {
+  var bloc = new WeatherCubit();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,7 +21,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
-      home: HomePage('Flutter Cubit learning'),
+      home: BlocProvider(
+        create: (context) => bloc, // WeatherBloc instance
+        child: HomePage('Flutter Cubit learning'),
+      ),
     );
   }
 }
@@ -32,6 +38,24 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
+      ),
+      body: BlocConsumer<WeatherCubit, WeatherState>(
+        listener: (context, state) {
+          return null;
+        },
+        builder: (context, state) {
+          if (state is WeatherInitial) {
+            return Text('WeatherInitial');
+          } else if (state is WeatherLoading) {
+            return Text('Loading');
+          } else if (state is WeatherLoaded) {
+            return Text('Loaded');
+          }
+
+          return Container(
+            child: Center(child: Text('WeatherInitial')),
+          );
+        },
       ),
     );
   }
