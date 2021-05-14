14b55fc37bb7b25b36e1ed6216dd62aaaa00cda8
Yukwok <billynyk@gmail.com>
2021-05-14 11:03:33 +0800

    remove bloc

--- bloc/newbloc_event.dart
+++
@@ -1,4 +0,0 @@
-part of 'newbloc_bloc.dart';
-
-@immutable
-abstract class NewblocEvent {}
--- bloc/newbloc_state.dart
+++
@@ -1,6 +0,0 @@
-part of 'newbloc_bloc.dart';
-
-@immutable
-abstract class NewblocState {}
-
-class NewblocInitial extends NewblocState {}
